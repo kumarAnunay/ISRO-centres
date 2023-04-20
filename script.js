1ca078c58cac08f1f5ce31680895b8ae617d6c06
kumarAnunay <kumaranunay12@gmail.com>
2023-04-20 15:11:53 +0530

    Updated

--- script.js
+++ script.js
@@ -1,17 +1,11 @@
-var data;
+let data;
 
-//Search Bar
 const searchButton = document.getElementById("search-button");
 const searchInput = document.getElementById("search-input");
 
-const api_url = "https://isro.vercel.app/api/centres";
-
-// Defining async function
-async function getapi(url) {
-  // Storing response
+async function getapi() {
+  const url = "https://isro.vercel.app/api/centres";
   const response = await fetch(url);
-
-  // Storing data in form of JSON
   data = await response.json();
   console.log(data);
   if (response) {
@@ -19,15 +13,12 @@ async function getapi(url) {
   }
   show(data);
 }
-// Calling that async function
-getapi(api_url);
 
-// Function to hide the loader
 function hideloader() {
-  document.getElementById("loading").style.display = "none";
   document.getElementById("loadingSpan").style.display = "none";
 }
-// Function to define innerHTML for HTML table
+
+
 function show(data) {
   let tab = `<tr>
         <th scope="col">S.no</th>
@@ -36,51 +27,43 @@ function show(data) {
 		<th scope="col">State</th>
 		</tr>`;
 
-  // Loop to access all rows
+
   for (let r of data.centres) {
     tab += `<tr class="row">
-    <td class = "table-data">${r.id} </td>
-	<td "table-data">${r.name} </td>
-	<td "table-data">${r.Place}</td>
-	<td "table-data">${r.State}</td>
-</tr>`;
+            <td class = "table-data">${r.id} </td>
+            <td "table-data">${r.name} </td>
+            <td "table-data">${r.Place}</td>
+            <td "table-data">${r.State}</td>
+            </tr>`;
   }
-  // Setting innerHTML as tab variable
+
   document.getElementById("isroCenters").innerHTML = tab;
 }
 
-//Function to set Flag
-var flag = "name";
 
-document.getElementById("city").addEventListener("click", () => {
-  removeFocus();
-  var element = document.getElementById("city");
-  element.classList.add("focus");
+let flag = "";
+
+function cityBtn() {
   flag = "city";
-});
+  searchData();
+};
 
-document.getElementById("state").addEventListener("click", () => {
-  removeFocus();
-  var element = document.getElementById("state");
-  element.classList.add("focus");
+function stateBtn() {
   flag = "state";
-});
+  searchData();
+};
 
-document.getElementById("name").addEventListener("click", () => {
-  debugger;
-  removeFocus();
-  var element = document.getElementById("name");
-  element.classList.add("focus");
+function centerBtn() {
   flag = "name";
-});
+  searchData();
+};
 
 console.log(flag);
 
-//function for searching center by name
 function searchData() {
   debugger;
   console.log(data);
-  var val = document.getElementById("search").value;
+  let val = document.getElementById("search").value;
   let tab = `<tr class="row">
         <th class="col">S.no</th>
 		<th class="col">Center Name</th>
@@ -97,10 +80,8 @@ function searchData() {
   }
 }
 
-//Funtion for searching center by name
 function searchByName(tab, val) {
   let i = 1;
-
   for (let r of data.centres) {
     if (r.name.toLowerCase().includes(val.toLowerCase())) {
       tab += `<tr class="row">
@@ -121,7 +102,6 @@ function searchByName(tab, val) {
   document.getElementById("isroCenters").innerHTML = tab;
 }
 
-//Funtion for searching center by state
 function searchByState(tab, val) {
   let i = 1;
   for (let r of data.centres) {
@@ -144,7 +124,6 @@ function searchByState(tab, val) {
   document.getElementById("isroCenters").innerHTML = tab;
 }
 
-//Funtion for searching center by city
 function searchByCity(tab, val) {
   let i = 1;
 
@@ -167,10 +146,4 @@ function searchByCity(tab, val) {
   document.getElementById("isroCenters").innerHTML = tab;
 }
 
-//function to remove focus class from all buttons
-function removeFocus() {
-  const allElements = document.querySelectorAll(".focus");
-  allElements.forEach((element) => {
-    element.classList.remove("focus");
-  });
-}
+getapi();
